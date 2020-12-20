eec9b28a25c97a82d3303da6c5f04e1264290f35
rapmaniaco <75867091+rapmaniaco@users.noreply.github.com>
2020-12-20 17:54:48 -0300

    Add files via upload

---
+++ 06_TF_ssd_semapu_opencv_video.py
@@ -0,0 +1,132 @@
+#python 06_TF_ssd_semapu_opencv_video.py
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
+import pathlib
+import tensorflow as tf
+import time
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
+
+tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
+
+import cv2
+
+# Enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+# Download and extract model
+def download_model(model_name):
+    base_url = 'http://download.tensorflow.org/models/object_detection/tf2/'
+    model_file = model_name + '.tar.gz'
+    model_dir = tf.keras.utils.get_file(fname=model_name,
+                                        origin=base_url + '/' + model_file,
+                                        untar=True)
+    print(str(model_dir))
+    return str(model_dir)
+
+#http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz
+
+
+MODEL_NAME = 'ssd_mobilenet_v2_2'
+#'centernet_hg104_1024x1024_coco17_tpu-32'
+PATH_TO_MODEL_DIR = download_model(MODEL_NAME)
+
+# Download labels file
+def download_labels(filename):
+    base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/data/'
+    label_dir = tf.keras.utils.get_file(fname=filename,
+                                        origin=base_url + filename,
+                                        untar=False)
+    label_dir = pathlib.Path(label_dir)
+    return str(label_dir)
+
+LABEL_FILENAME = 'mscoco_label_map.pbtxt'
+PATH_TO_LABELS = download_labels(LABEL_FILENAME)
+
+PATH_TO_SAVED_MODEL = PATH_TO_MODEL_DIR + "/saved_model"
+
+print('Loading model...', end='')
+start_time = time.time()
+
+# Load saved model and build the detection function
+detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Done! Took {} seconds'.format(elapsed_time))
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
+                                                                    use_display_name=True)
+
+
+
+
+
+cap = cv2.VideoCapture('../Bangkok-30945.mp4')
+
+ret=True
+
+while(ret==True):
+    
+    ret, image_np = cap.read()
+    
+    if (ret==True):
+
+        #print('Running inference for {}... '.format(image_path), end='')
+
+        #image_np = load_image_into_numpy_array(image_path)
+
+        # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+        input_tensor = tf.convert_to_tensor(image_np)
+        # The model expects a batch of images, so add an axis with `tf.newaxis`.
+        input_tensor = input_tensor[tf.newaxis, ...]
+
+        # input_tensor = np.expand_dims(image_np, 0)
+        detections = detect_fn(input_tensor)
+
+        # All outputs are batches tensors.
+        # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+        # We're only interested in the first num_detections.
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy()
+                       for key, value in detections.items()}
+        detections['num_detections'] = num_detections
+
+        # detection_classes should be ints.
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+        image_np_with_detections = image_np.copy()
+
+        viz_utils.visualize_boxes_and_labels_on_image_array(
+              image_np_with_detections,
+              detections['detection_boxes'],
+              detections['detection_classes'],
+              detections['detection_scores'],
+              category_index,
+              use_normalized_coordinates=True,
+              max_boxes_to_draw=200,
+              min_score_thresh=.50,
+              agnostic_mode=False)
+
+        plt.figure()
+        plt.imshow(image_np_with_detections)
+        #print('Done')
+    cv2.imshow('Frame', image_np_with_detections)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+#plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# sphinx_gallery_thumbnail_number = 2
